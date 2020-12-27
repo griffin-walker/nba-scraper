6ac54bbb2a4e723b2f507afe219d4544bff186ff
griffinwalker2 <griffin.walker@mac.com>
2020-12-27 11:49:24 -0500

    Check in get_season functionality

--- shcrape/scrape.py
+++ shcrape/scrape.py
@@ -1,29 +1,13 @@
-""""""
+"""Module for scraping basketball reference for game stats."""
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
+import json
+from tqdm import tqdm
 
 
 BASE_URL = "https://basketball-reference.com"
-path = "/boxscores/202012230CHI.html"
 HEADERS = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
-
-SEASONS = list(range(2011, 2022))
-
-# <div class="filter"> # grab a.s and their href
-
-
-def iterate_season_months(month_refs: list):
-    return
-
-
-def parse_season_schedule(season_soup: BeautifulSoup):
-    return
-
-
-def get_schedule(season: str):
-    """"""
-    return
 
 
 def get_and_soup(extension: str) -> BeautifulSoup:
@@ -42,6 +26,71 @@ def get_and_soup(extension: str) -> BeautifulSoup:
         return response.status_code
 
 
+def get_season_months(season: int) -> list:
+    season_ext = f"/leagues/NBA_{season}_games.html"
+    season_soup = get_and_soup(extension=season_ext)
+    season_months = season_soup.find("div", {"class": "filter"}).find_all("a")
+    month_extensions = [
+        {"month": a.contents[0], "extension": a.get("href")} for a in season_months
+    ]
+    return month_extensions
+
+
+def parse_schedule_row(row: element.Tag) -> dict:
+    reference_ext = (
+        row.find("td", {"data-stat": "box_score_text"}).find("a").get("href")
+    )
+    home_team = row.find("td", {"data-stat": "home_team_name"}).find("a").contents[0]
+    away_team = row.find("td", {"data-stat": "visitor_team_name"}).find("a").contents[0]
+    visitor_id = (
+        row.find("td", {"data-stat": "visitor_team_name"}).get("csk").split(".")[0]
+    )
+    home_pts = row.find("td", {"data-stat": "home_pts"}).contents[0]
+    away_pts = row.find("td", {"data-stat": "visitor_pts"}).contents[0]
+    row_dict = {
+        "reference_extension": reference_ext,
+        "home": home_team,
+        "away": away_team,
+        "home_pts": home_pts,
+        "away_pts": away_pts,
+        "away_id": visitor_id,
+    }
+    return row_dict
+
+
+def parse_monthly_schedule(month_soup: BeautifulSoup):
+    trs = month_soup.find_all("tr")
+    parsed_rows = []
+    for tr in trs:
+        try:
+            if tr.find("th", {"data-stat": "date_game"}).contents == ["Date"]:
+                continue
+            else:
+                parsed_rows.append(parse_schedule_row(tr))
+        except ValueError:
+            raise Exception("Value Error")
+    return parsed_rows
+
+
+def get_season_schedule(season: int):
+    season_months = get_season_months(season=season)
+    games = []
+    for season_month in tqdm(season_months):
+        month_soup = get_and_soup(extension=season_month["extension"])
+        month_games = parse_monthly_schedule(month_soup)
+        games.extend(month_games)
+    return games
+
+
+def get_seasons(seasons=range(2011, 2022)):
+    season_game_map = {}
+    for season in tqdm(seasons):
+        games = get_season_schedule(season=season)
+        season_game_map[season] = games
+    return season_game_map
+
+
 if __name__ == "__main__":
-    soup = get_and_soup(extension=path)
-    print(soup)
+    season_game_map = get_seasons()
+    with open("season_game_map.json", "w") as outfile:
+        json.dump(season_game_map, outfile)
